6dd10f9030201b0cfe89f447200c47c6671b1438
jpablobr <xjpablobrx@gmail.com>
2011-11-06 00:13:27 +1000

    changing the subject message to include the store email

--- app/mailer/contact_mailer.rb
+++ app/mailer/contact_mailer.rb
@@ -2,9 +2,9 @@ class ContactMailer < ActionMailer::Base
   helper "spree/base"
 
   def message_email(message)
-    subject = "#{Spree::Config[:site_name]} - #{t('message_from')} #{message.email}"
+    subject = "#{Spree::Config[:site_name]} - #{t('message_from')} #{Spree::Config[:contact_form_to]}"
 
     @message = message
-    mail(:to => message.email, :subject => subject, :bcc => Spree::Config[:contact_form_to])
+    mail(:to => message.email, :subject => subject, :reply_to => Spree::Config[:contact_form_to])
   end
 end
